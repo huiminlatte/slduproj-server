217ccf93964b9e53d6f79abbc0f3e515d1022eeb
huiminlatte <36180708+huiminlatte@users.noreply.github.com>
2019-06-07 23:02:20 +0800

    updated codes 7/6/2019

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database: 'mydb'
+    database: 'mydb'npm
 });
 
 connection.connect((error) => {
